50d05c41b81008a4a0c477fd3a5093a29cd9b924
davidoliva20hn <davidoliva20@unitec.edu>
2024-05-03 15:37:01 -0600

    Tercer Commit Ejecicio uno terminado

--- Lab3P3_DavidOliva.cpp
+++ Lab3P3_DavidOliva.cpp
@@ -6,47 +6,77 @@
 #include <stdlib.h>    
 #include <time.h>  
 using namespace std;
-int ElecciónIntercalada(vector <int> Vector,int Entrar) {
-	int Valor = ElecciónIntercalada(Vector);
-	if (Entrar) {
-		Entrar = 0;
-		return 1;
+void ImprimirVector(vector <int> Vector) {
+	for (int i = 0; i < Vector.size(); i++) {
+		cout << (i == 0 ? "[" : "") << " " << Vector[i] << (i == Vector.size() - 1 ? " ]\n" : "");
 	}
-	if (Vector.size()-1 == 0) {
+}
+int ElecciónIntercalada(vector <int> Vector, int Entrar, int Valor, int Detener) {
+	if (Vector.size() % 2 == 1) {
+		Detener = (Vector.size() / 2) + 1;
+	}
+	else {
+		Detener = Vector.size() / 2;
+	}
 
+	if (Entrar) {
+		return ElecciónIntercalada(Vector, 0, 1, 0);
 	}
-	else if (Valor ==1){
-		int Contador = 0;
-		for (size_t i = 0; i < ; i++) {
-			Vector.erase(Vector.begin()+Contador);
-			Contador++;
-		}
-		return 2;
+
+	if (Vector.size() - 1 == 0) {
+		cout << "Último sobreviviente : [" << Vector[0] << "]" << "\n";
+		return 0;
 	}
-	else if (Valor ==2) {
+	else if (Valor == 1) {
 		int Contador = 0;
-		for (size_t i = 0; i < Vector; i++) {
-			Vector.erase(Vector.begin() + Contador);
-			Contador++;
+		for (size_t i = 0; i < Vector.size(); i++) {
+			if (Detener == Contador)
+			{
+			}
+			if (Detener != Contador) {
+				Vector.erase(Vector.begin() + Contador);
+				Contador++;
+			}
+			else {
+				break;
+			}
 		}
-		return 1;
+		ImprimirVector(Vector);
+		ElecciónIntercalada(Vector, 0, 2, 0);
+	}
+	else if (Valor == 2) {
+		int Contador = Vector.size() - 1;
+		for (int i = Vector.size() - 1; i >= 0; i--) {
+			if (Contador >= 0) {
+				Vector.erase(Vector.begin() + Contador);
+				Contador -= 2;
+			}
+		}
+		ImprimirVector(Vector);
+		return ElecciónIntercalada(Vector, 0, 1, 0);
 	}
 }
 void ejercicio1() {
+	srand(time(NULL));
 	cout << "El último sobreviviente." << endl << endl;
 	cout << "Ingrese el tamaño del vector:";
 	int TamañoVector;
 	cin >> TamañoVector;
+	while (TamañoVector < 5 || TamañoVector >15)
+	{
+		cout << "El valor esta fuera de rango." << "\n";
+		cout << "Ingrese el tamaño del vector:";
+		cin >> TamañoVector;
+	}
 	vector <int> ListaVectores;
-	for (size_t i = 0; i < TamañoVector; i++){
-		int NumeroAleatorio= 1 + rand() % 10;;
+	for (size_t i = 0; i < TamañoVector; i++) {
+		int NumeroAleatorio = 1 + rand() % 10;;
 		ListaVectores.push_back(NumeroAleatorio);
 	}
 	for (int i = 0; i < TamañoVector; i++) {
-		cout << (i == 0 ? "Vector generado : [" : "") << " " << ListaVectores[i] << (i == TamañoVector-1 ? "]\n" : "");
+		cout << (i == 0 ? "Vector generado : [" : "") << " " << ListaVectores[i] << (i == TamañoVector - 1 ? " ]\n" : "");
 	}
-
-	cout << "Último sobreviviente : [" << "]";
+	ElecciónIntercalada(ListaVectores, 1, 0, 0);
 }
 void ejercicio2() {
 
@@ -64,7 +94,7 @@ void menu() {
 			ejercicio1();
 			break;
 		case 2:
-			ejercicio1();
+			ejercicio2();
 			break;
 		case 3:
 			cout << " ";
